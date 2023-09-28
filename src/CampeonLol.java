6a1449f04e9842b7dd3efe7903b1e181361cf842
KidoMARU09 <breinerasencio16@gmail.com>
2023-09-28 00:21:40 -0500

    Se comento el código.

--- src/CampeonLol.java
+++ src/CampeonLol.java
@@ -1,14 +1,28 @@
 public class CampeonLol {
+    /**
+     * Atributos:
+     */
     String personaje;
     String habilidad;
     String HabilidadDefinitiva;
     CampeonLol(String personaje, String habilidad, String  HabilidadDefinitiva){
+        /**
+         *@param personaje Nombre del personaje
+         *@param habilidad cuales son sus habilidades basicas
+         *@param HabilidadDefinitiva habilidad que se caracteriza por su daño
+         *Complejidad temporal: O(1) Complejidad Constante
+         */
         this.habilidad = habilidad;
         this.personaje = personaje;
         this. HabilidadDefinitiva =  HabilidadDefinitiva;
 
     }
+
+    /**
+     * Este método imprime el nombre del personaje, sus habilidades, y la habilidad definitiva
+     */
     void CaracteristicaCampeon(){
-        System.out.printf("El personaje es " + personaje + " y sus habilidad es " + habilidad + " y Su habilidad definitiva es " + HabilidadDefinitiva);
+        System.out.println("El personaje es " + personaje + " y sus habilidad es " + habilidad);
+        System.out.println("Su habilidad definitiva es " + HabilidadDefinitiva);
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
-
+/**
+ * Primera clase.
+ */
 public class Main {
+    /**
+     * Primer método que se ejecuta
+     * @param args
+     *
+     * Complejidad temporal: O(1) Complejidad Constante
+     */
     public static void main(String[] args) {
         CampeonLol lux = new CampeonLol("Lux", "Inmoviliza(Q), Proteccion(w), Ataque en área(E)", "Ataque rayo de luz con máximo daño.");
         lux.CaracteristicaCampeon();
